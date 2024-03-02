f97f25e242a51b98a17d016bc8b202f8dc0c9cfb
Tade77 <adekunletaofeeq@mail.com>
2024-03-02 12:38:14 +0100

    feat: Context and ThemeProvider set up

--- src/ThemProvider/ThemeProvider.tsx
+++ src/ThemProvider/ThemeProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import { useEffect, useState } from "react";
+import ThemeContext from "../Context/ThemeContext";
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const ThemeFromStorage: boolean =
@@ -9,4 +10,22 @@ const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
       : false;
 
   const [darkTheme, setDarkTheme] = useState<boolean>(ThemeFromStorage);
+  const [renderComponent, setRenderComponent] = useState(false);
+
+  useEffect(() => {
+    setRenderComponent(true);
+  }, []);
+
+  if (!renderComponent) return <></>;
+
+  return (
+    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+      <div className={`${darkTheme ? "dark" : ""} min-h-screen`}>
+        <div className=" dark:text-white dark:bg-black text-[#1E1E1E]">
+          {children}
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
 };
+export default ThemeProvider;
